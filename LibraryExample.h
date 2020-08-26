121c437071040bc5a4f4a0f14f4c8f8173d28942
hapiarin <70111669+hapiarin@users.noreply.github.com>
2020-08-26 12:32:10 +0200

    Delete LibraryExample.h

--- LibraryExample.h
+++
@@ -1,8 +0,0 @@
-class example {
-private:        //W private dajemy wszystkie inty,stringi oraz inne zmienne publiczne
-  int red; //deklarujemy zmienna aby ustawic ja na publiczna
-  int pin; //deklarujemy zmienna pin
-public:         // W public deklarujemy tylko void'y oraz nazwy spoza void setup()
-  example(int r);  //Deklarowanie nazwą na zewnatrz (spoza void'a np setup) robi sie bez void
-  void Blink13(int czas); //Deklarujemy sobie void'a blink z zmienna czas           // deklarowanie funkcji/voida ktory ma byc w voidzie (np w setup) robi sie z void
-};
